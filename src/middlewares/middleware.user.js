9f8a3003c6068127ca3d6bf1de8a58f36ec182c6
Roberto Luna <robertocgw@gmail.com>
2022-09-29 10:43:03 -0300

    feat: validação para criação de novo usuário no middleware user

--- src/middlewares/middleware.user.js
+++ src/middlewares/middleware.user.js
@@ -7,4 +7,23 @@ const validateLogin = (req, res, next) => {
   next();
 };
 
-module.exports = { validateLogin };
+const validateNewUser = (req, res, next) => {
+  const { displayName, email, password } = req.body;
+
+  if (displayName < 8) {
+    return res.status(400)
+    .json({ message: '"displayName" length must be at least 8 characters long' });
+  }
+  if (password < 6) {
+    return res.status(400)
+    .json({ message: '"password" length must be at least 6 characters long' });
+  }
+  const regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+  const validateEmail = regex.test(email);
+  if (!validateEmail) return res.status().json({ message: '"email" must be a valid email' }); 
+};
+
+module.exports = {
+  validateLogin,
+  validateNewUser,
+};
